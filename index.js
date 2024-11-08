259b97c00a1c86686cf600e2ccce3f14ef2e4151
TomLindblad <168995337+TomLindblad@users.noreply.github.com>
2024-11-08 22:24:56 +0100

    changed so the color changes happen with add/remove classes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let myWeaponChoice;
 let enemyWeaponChoice;
-const weaponlist = ["rock", "paper", "scissor"];
+const weaponlist = ["ROCK", "PAPER", "SCISSOR"];
 
 let timesWon = 0;
 let timesLost = 0;
@@ -11,47 +11,41 @@ function compareWeapons(myWeapon, enemyWeapon){
     enemyWeaponChoice = enemyWeapon
 
     switch (enemyWeaponChoice){
-        case "rock": document.getElementById("enemyIcon").className = "fa-regular fa-hand-back-fist"; 
+        case "ROCK": document.getElementById("enemyIcon").className = "fa-regular fa-hand-back-fist"; 
             break;
-        case "paper": document.getElementById("enemyIcon").className = "fa-regular fa-hand"; 
+        case "PAPER": document.getElementById("enemyIcon").className = "fa-regular fa-hand"; 
             break;
-        case "scissor": document.getElementById("enemyIcon").className = "fa-regular fa-hand-scissors"; 
+        case "SCISSOR": document.getElementById("enemyIcon").className = "fa-regular fa-hand-scissors"; 
             break;
         default: document.getElementById("enemyIcon").className = "fa-solid fa-question"; ;
     }
 
-    console.log(myWeapon);
-    console.log(enemyWeapon);
-    console.log(timesWon);
-    console.log(timesLost);
-    checkHighscore()
-    console.log(winStreak);
-
     if(myWeapon == enemyWeapon){
-        document.getElementById("resultText").textContent = `It's a tie!`;
+        document.getElementById("resultText").textContent = `IT'S A TIE!`;
+        setTimeout(reset, 800);
     }
 
-    else if (myWeapon == "rock" && enemyWeapon == "scissor"){
+    else if (myWeapon == "ROCK" && enemyWeapon == "SCISSOR"){
         win();
     }
 
-    else if (myWeapon == "paper" && enemyWeapon == "rock"){
+    else if (myWeapon == "PAPER" && enemyWeapon == "ROCK"){
         win();
     }
 
-    else if (myWeapon == "scissor" && enemyWeapon == "paper"){
+    else if (myWeapon == "SCISSOR" && enemyWeapon == "PAPER"){
         win();
     }
 
-    else if (myWeapon == "rock" && enemyWeapon == "paper"){
+    else if (myWeapon == "ROCK" && enemyWeapon == "PAPER"){
         lose();
     }
 
-    else if (myWeapon == "paper" && enemyWeapon == "scissor"){
+    else if (myWeapon == "PAPER" && enemyWeapon == "SCISSOR"){
         lose();
     }
 
-    else if (myWeapon == "scissor" && enemyWeapon == "rock"){
+    else if (myWeapon == "SCISSOR" && enemyWeapon == "ROCK"){
         lose();
     }
 }
@@ -62,21 +56,33 @@ function getEnemyWeapon(){
 }
 
 function checkHighscore(){
-    timesWon > winStreak ? winStreak = timesWon : winStreak = winStreak;
+    timesWon > winStreak ? winStreak = timesWon : winStreak = winStreak;            // DO SOMETHING WITH THIS? 
 }
 
 function win(){
-    document.getElementById("enemycard").style.background = "red";
-    document.getElementById(myWeaponChoice).style.background = "green";
+    document.getElementById("enemycard").style.background = "rgb(214, 38, 38)";
+    document.getElementById(myWeaponChoice).classList.remove("cardcolor");
+    document.getElementById(myWeaponChoice).classList.add("win");
+    document.getElementById("resultText").textContent = `${myWeaponChoice} BEATS ${enemyWeaponChoice}`;
     timesWon++
-    document.getElementById("resultText").textContent = `${myWeaponChoice} beats ${enemyWeaponChoice}`;
     document.getElementById("winText").textContent = `Times won: ${timesWon}`;
+    setTimeout(reset, 800);
 }
 
 function lose(){
-    document.getElementById("enemycard").style.background = "green";
-    document.getElementById(myWeaponChoice).style.background = "red";
+    document.getElementById("enemycard").style.background = "rgb(13, 209, 62)";
+    document.getElementById(myWeaponChoice).classList.remove("cardcolor");
+    document.getElementById(myWeaponChoice).classList.add("lose");
+    document.getElementById("resultText").textContent = `${enemyWeaponChoice} BEATS ${myWeaponChoice}`;
     timesLost++
-    document.getElementById("resultText").textContent = `${enemyWeaponChoice} beats ${myWeaponChoice}`;
     document.getElementById("loseText").textContent = `Times lost: ${timesLost}`;
+    setTimeout(reset, 800);
+}
+
+function reset(){
+    document.getElementById("enemycard").style.background = "rgb(230, 230, 134)";
+    document.getElementById(myWeaponChoice).classList.add("cardcolor");
+    document.getElementById(myWeaponChoice).classList.remove("win");
+    document.getElementById(myWeaponChoice).classList.remove("lose");
+    document.getElementById("enemyIcon").className = "fa-solid fa-question";
 }
